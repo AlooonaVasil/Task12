b9829f0f228ae44a51429cf7eef49477daded561
Алёна Алёна <alenchaeto1@outlook.com>
2022-12-27 10:27:42 +0300

    Добавьте файлы проекта.

---
+++ Task12/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    static class Circle
+    {
+        public static double GetLength(double r)
+        {
+            return (2 * Math.PI * r);
+        }
+        public static double GetSquare(double r)
+        {
+            return (Math.PI * Math.Pow(r, 2));
+        }
+        public static string PointStatus(double x, double y, double x0, double y0, double r)
+        {
+            var d = Math.Sqrt(Math.Pow(x - x0, 2) + Math.Pow(y - y0, 2));
+            return d < r ? "Точка принадлежит кругу" : d == r ? "Точка на окружности" : "Точка вне круга";
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности ");
+            var r = Convert.ToDouble(Console.ReadLine());
+            var length = Circle.GetLength(r);
+            var square = Circle.GetSquare(r);
+            Console.WriteLine($"Длина окружности = {length:f2} \nПлощадь круга = {square:f2}");
+            Console.WriteLine();
+
+            Console.WriteLine("Введите координаты центра круга (x0 и y0)");
+            var x0 = Convert.ToDouble(Console.ReadLine());
+            var y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты точки (x и y)");
+            var x = Convert.ToDouble(Console.ReadLine());
+            var y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(Circle.PointStatus(x, y, x0, y0, r));
+            Console.ReadKey();
+        }
+    }
+}
